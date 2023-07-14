8af4c0bec78abe72196073f5ef93d9415bdc111f
Nwali Philip <kwissboy@gmail.com>
2023-07-14 09:40:10 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Function Entry point
+ * Return: 0 to show code executed Successfully
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
