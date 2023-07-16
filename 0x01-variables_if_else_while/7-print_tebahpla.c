1bf65fa088ad2b70abd69d5f16335eb0f1ca7852
Nwali Philip <kwissboy@gmail.com>
2023-07-15 17:40:05 -0700

    c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Function Entry point
+ * Return: 0 to show code executed Successfully
+ */
+
+int main(void)
+{
+char c = 'z';
+while (c >= 'a')
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
+}
