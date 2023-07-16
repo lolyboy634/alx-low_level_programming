25598548a714191635670946dba50b53d1bf9771
Nwali Philip <kwissboy@gmail.com>
2023-07-15 17:29:07 -0700

    c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Function Entry point
+ * Return: 0 to show code executed Successfully
+ */
+
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
