c94414b2fa2ec7c24ce102e01f652f3b5a33dcae
Nwali Philip <kwissboy@gmail.com>
2023-07-15 17:38:05 -0700

    c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Function Entry point
+ * Return: 0 to show code executed Successfully
+ */
+
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+putchar(n + '0');
+n++;
+}
+putchar('\n');
+return (0);
+}
