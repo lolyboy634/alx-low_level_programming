3f75d58ecd2422c0c6fdbbbda1c0e59717d295d1
Nwali Philip <kwissboy@gmail.com>
2023-07-15 17:36:02 -0700

    c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Function Entry point
+ * Return: 0 to show code executed Successfully
+ */
+
+int main(void)
+{
+int a;
+
+for (a = 0; a < 10; a++)
+printf("%d", a);
+putchar('\n');
+return (0);
+}
